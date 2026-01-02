c362c33da72d64e29edbdd20333c6c3cc781d66a
Venkatesh Burra <burravenkatesh284@gmail.com>
2026-01-02 22:18:43 +0530

    updated navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,25 +1,26 @@
 import { ShoppingBagIcon, ShoppingCartIcon } from "lucide-react";
 import { Link, useResolvedPath } from "react-router-dom";
 import ThemeSelector from "./ThemeSelector";
+import { useProductStore } from "../store/useProductStore";
 
 export function Navbar() {
 
     const { pathname} = useResolvedPath();
     const isHomePage = pathname === "/";
+    const { products } = useProductStore();
     
 
   return (
-    <div className="bg-base-100/80 backdrop-blur-lg border-b border-base-content/10 sticky top-0 z-50">
-      <div className="max-w-7xl mx-auto">
+    <div className="sticky top-0 z-50 border-b bg-base-100/80 backdrop-blur-lg border-base-content/10">
+      <div className="mx-auto max-w-7xl">
         <div className="navbar px-4 min-h-[4rem] justify-between">
           {/* LOGO */}
           <div className="flex-1 lg:flex-none">
-            <Link to="/" className="hover:opacity-80 transition-opacity">
+            <Link to="/" className="transition-opacity hover:opacity-80">
               <div className="flex items-center gap-2">
                 <ShoppingCartIcon className="size-9 text-primary" />
                 <span
-                  className="font-semibold font-mono tracking-widest text-2xl 
-                    bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary"
+                  className="font-mono text-2xl font-semibold tracking-widest text-transparent bg-clip-text bg-gradient-to-r from-primary to-secondary"
                 >
                   POSGRESTORE
                 </span>
@@ -33,10 +34,10 @@ export function Navbar() {
           {/* RIGHT SECTION */}
               {isHomePage && (
               <div className="indicator">
-                <div className="p-2 rounded-full hover:bg-base-200 transition-colors">
+                <div className="p-2 transition-colors rounded-full hover:bg-base-200">
                   <ShoppingBagIcon className="size-5" />
                   <span className="badge badge-sm badge-primary indicator-item">
-                    8
+                    {products.length}
                   </span>
                 </div>
               </div>
